b13fd1db894a989b400e2ca8de24fe1fd4764466
sprisao <whdtmek@gmail.com>
2023-11-17 10:33:01 +0900

    Edit : Min-Width=1024px, Max-Width=1920px, 가운데 설정

--- app/page.js
+++ app/page.js
@@ -1,17 +1,17 @@
 'use client'
 import UserTable from "./components/UserTable";
 import {useState} from "react";
-import { faker } from '@faker-js/faker';
+import {faker} from '@faker-js/faker';
 
 
-export function createRandomUser(){
+export function createRandomUser() {
     return {
         userId: faker.datatype.uuid(),
         username: faker.internet.userName(),
         email: faker.internet.email(),
         // avatar: faker.image.avatar(),
         password: faker.internet.password(),
-        birthdate: faker.date.birthdate({ min: 18, max: 65, mode: 'age' }), // age 옵션 사용
+        birthdate: faker.date.birthdate({min: 18, max: 65, mode: 'age'}), // age 옵션 사용
         registeredAt: faker.date.past(),
     };
 }
@@ -22,7 +22,7 @@ export default function Page() {
     const [isGenerated, setIsGenerated] = useState(false);
 
     const generateUsers = () => {
-        const newUsers = Array.from({ length: 50000 }, () => createRandomUser());
+        const newUsers = Array.from({length: 50000}, () => createRandomUser());
         setUsers(newUsers);
         setIsGenerated(true);
     };
@@ -32,7 +32,7 @@ export default function Page() {
     };
 
     return (
-        <div>
+        <div className="bg-amber-200 mainContainer">
             <h1 className="text-3xl font-bold">User Table</h1>
             <button
                 className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 generate-users-button"
@@ -47,7 +47,7 @@ export default function Page() {
             >
                 Show Table
             </button>
-            {showTable && <UserTable users={users} />}
+            {showTable && <UserTable users={users}/>}
         </div>
     );
 }
