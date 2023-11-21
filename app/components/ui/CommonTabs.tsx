59ec9f1eecfb3b5f5b89074118400f9ed34372b5
sprisao <whdtmek@gmail.com>
2023-11-21 17:50:17 +0900

    기본 UI 디테일 수정  - 이력조회 섹션 > 탭 디자인 디테일 변경

--- app/components/ui/CommonTabs.tsx
+++ app/components/ui/CommonTabs.tsx
@@ -6,14 +6,16 @@ export default function CommonTabs({
     return (
         <div className="flex h-full text-sm items-end mx-0.5 ">
             <button
-                className={`px-5 py-1 ${activeTab === 'tab1' ? 'text-blue-500 bg-blue-200 rounded-t-md  border-blue-200 ' : ''}`}
+                className={`px-5 py-1 ${activeTab === 'tab1' ? 'border text-white bg-blue-400 rounded-t-md  border-blue-200 ' : ''}`}
                 onClick={() => setActiveTab('tab1')}>고객상담이력
             </button>
-            <button className={`px-5 py-1 ${activeTab === 'tab2' ? 'text-blue-500 bg-blue-200 rounded-t-md  border-blue-200 ' : ''}`}
-                    onClick={() => setActiveTab('tab2')}>미납상담이력
+            <button
+                className={`px-5 py-1 ${activeTab === 'tab2' ? 'text-white bg-blue-400 rounded-t-md  border-blue-200 ' : ''}`}
+                onClick={() => setActiveTab('tab2')}>미납상담이력
             </button>
-            <button className={`px-5 py-1 ${activeTab === 'tab3' ? 'text-blue-500 bg-blue-200 rounded-t-md  border-blue-200 ' : ''}`}
-                    onClick={() => setActiveTab('tab3')}>SMS전송이력
+            <button
+                className={`px-5 py-1 ${activeTab === 'tab3' ? 'text-white bg-blue-400 rounded-t-md  border-blue-200 ' : ''}`}
+                onClick={() => setActiveTab('tab3')}>SMS전송이력
             </button>
         </div>
     );
