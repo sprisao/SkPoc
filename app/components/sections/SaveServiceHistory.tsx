509ceb493e7723fb75bba3ddb4f66ab08666fe1e
sprisao <whdtmek@gmail.com>
2023-11-22 13:00:07 +0900

    디자인 리터치 & 요소 컴포넌트화  - 상담이력저장 섹션 디자인 조정  - TextAreaBox.tsx 컴포넌트 추가

--- app/components/sections/SaveServiceHistory.tsx
+++ app/components/sections/SaveServiceHistory.tsx
@@ -1,6 +1,9 @@
 /*상담이력저장*/
 import SectionTitle from "@/app/components/ui/SectionTitle";
 import CommonButton from "@/app/components/ui/CommonButton";
+import SelectBox from "@/app/components/ui/SelectBox";
+import InputBox from "@/app/components/ui/InputBox";
+import TextAreaBox from "@/app/components/ui/TextAreaBox";
 
 const SaveServiceHistory = () => {
     return (
@@ -10,18 +13,22 @@ const SaveServiceHistory = () => {
                 <div className="flex flex-col space-y-1">
                     <div className=" grid grid-cols-5 gap-2">
                         <p className="shrink-0 text-right rounded-s">검색</p>
-                        <div className="flex flex-row col-span-4 justify-between">
-                            <input className="border-2"></input>
+                        <div className="flex flex-row col-span-4 justify-between space-x-2">
+                            <InputBox className=""
+                                      type="text"
+                                      placeholder=""
+                            />
                             <CommonButton>조회</CommonButton>
                         </div>
                     </div>
                     <div className="grid grid-cols-5 gap-2">
                         <p className="shrink-0 text-right">메모</p>
-                        <input className="col-span-4 border-2 h-36 rounded-s"/>
+                        <TextAreaBox className="col-span-4 border-2 h-36 rounded-s"
+                        />
                     </div>
                 </div>
                 <div className="">
-                    <input className="border-2 w-full h-full rounded-s"/>
+                    <TextAreaBox className="border-2 w-full h-full rounded-s"/>
                 </div>
                 <div>
                 </div>
@@ -33,7 +40,10 @@ const SaveServiceHistory = () => {
                             통화자명
                         </p>
                     </div>
-                    <input className="w-full bg-pink-100 grow border-2 rounded-sm"/>
+                    <InputBox className=""
+                              type="text"
+                              placeholder=""
+                    />
                 </div>
                 <div className="flex flex-row text-sm space-x-1">
                     <div className="flex items-center justify-end shrink-0">
@@ -41,7 +51,10 @@ const SaveServiceHistory = () => {
                             연락처
                         </p>
                     </div>
-                    <input className="w-full bg-pink-100 grow border-2 rounded-sm"/>
+                    <InputBox className=""
+                              type="text"
+                              placeholder=""
+                    />
                 </div>
                 <div className="flex flex-row text-sm space-x-1">
                     <div className="flex items-center justify-end shrink-0">
@@ -49,11 +62,13 @@ const SaveServiceHistory = () => {
                             접촉구분
                         </p>
                     </div>
-                    <select className="grow bg-pink-100 border-2 rounded-sm">
+                    <SelectBox
+                        className=""
+                    >
                         <option></option>
                         <option>1</option>
                         <option>2</option>
-                    </select>
+                    </SelectBox>
                 </div>
                 <div className="flex flex-row text-sm space-x-1">
                     <div className="flex items-center justify-end shrink-0">
@@ -61,11 +76,13 @@ const SaveServiceHistory = () => {
                             관계
                         </p>
                     </div>
-                    <select className="grow bg-pink-100 border-2 rounded-sm">
+                    <SelectBox
+                        className=""
+                    >
                         <option></option>
                         <option>1</option>
                         <option>2</option>
-                    </select>
+                    </SelectBox>
                 </div>
                 <div className="col-end-6">
                     <CommonButton>
--- app/components/ui/TextAreaBox.tsx
+++ app/components/ui/TextAreaBox.tsx
@@ -1,14 +1,12 @@
 import {cn} from "@/lib/utils";
 
-export default function InputBox({className, type, placeholder}) {
+export default function TextAreaBox({className}) {
     return (
-        <input
+        <textarea
             className={cn(
                 "w-full flex flex-row px-2 py-1 bg-gray-50 border-2 h-6 pb-[5px] text-left align-top ",
                 className
             )}
-            type={type}
-            placeholder={placeholder}
         />
     )
 }
