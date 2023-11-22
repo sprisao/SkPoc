1231ed2f1c0d3fcedff3768eec2808f1a2bcbc1e
sprisao <whdtmek@gmail.com>
2023-11-22 15:01:24 +0900

    리팩터링  - pages.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import {faker} from '@faker-js/faker';
 import {CustomerServiceInfoData} from "@/app/components/tables/CustomerServiceInfo/columns_customerServiceInfo";
 import CustomerServiceInfo from "@/app/components/sections/CustomerServiceInfo";
 import SearchCustomerService from "@/app/components/sections/SearchCustomerService";
@@ -7,25 +6,11 @@ import PaymentInformation, {PaymentInformationData} from "@/app/components/secti
 import SaveServiceHistory from "@/app/components/sections/SaveServiceHistory";
 import CustomerServiceDetail from "@/app/components/sections/CustomerServiceDetail";
 import {BillingInfoData} from "@/app/components/tables/BillingInfo/columns_billingInfo";
-import WidthDisplay from "@/app/utils/WidthDisplay";
-import SectionTitle from "@/app/components/ui/SectionTitle";
 import History from "@/app/components/sections/History";
 import {
     CustomerServiceHistoryData
 } from "@/app/components/tables/CustomerServiceHistory/columns_customerServiceHistory";
 
-
-// export function createRandomUser() {
-//     return {
-//         userId: faker.datatype.uuid(),
-//         username: faker.internet.userName(),
-//         email: faker.internet.email(),
-//         // avatar: faker.image.avatar(),
-//         password: faker.internet.password(),
-//         birthdate: faker.date.birthdate({min: 18, max: 65, mode: 'age'}), // age 옵션 사용
-//         registeredAt: faker.date.past(),
-//     };
-// }
 
 async function getCustomerInfoData(): Promise<CustomerServiceInfoData[]> {
     // Fetch data from your API here.
@@ -481,19 +466,6 @@ async function getCustomerServiceDetailData(): Promise<CustomerServiceHistoryDat
 }
 
 export default async function Page() {
-    // const [users, setUsers] = useState([]);
-    // const [showTable, setShowTable] = useState(false);
-    // const [isGenerated, setIsGenerated] = useState(false);
-    //
-    // const generateUsers = () => {
-    //     const newUsers = Array.from({length: 50000}, () => createRandomUser());
-    //     setUsers(newUsers);
-    //     setIsGenerated(true);
-    // };
-    //
-    // const handleShowTable = () => {
-    //     setShowTable(true);
-    // };
 
     const customerServiceInfoData = await getCustomerInfoData()
     const billingInfoData = await getBillingInfoData()
@@ -504,11 +476,6 @@ export default async function Page() {
 
     return (
         <div className="flex flex-col mainContainer px-3 space-y-5">
-            <div className="flex w-full flex-row space-x-2 justify-between m-1">
-                <div className="w-full border rounded-sm overflow-clip ">
-                    <SectionTitle title="고객상담"/>
-                </div>
-            </div>
 
             {/*고객상담관리 조회 Container*/}
             <SearchCustomerService/>
@@ -516,7 +483,7 @@ export default async function Page() {
             {/*고객상담 정보*/}
             <CustomerServiceInfo data={customerServiceInfoData}/>
 
-            {/*고객상담 상세정보, 청구정보, 납부정보, 상담이력저장 섹션ㅐ*/}
+            {/*고객상담 상세정보, 청구정보, 납부정보, 상담이력저장 섹션*/}
             <div className="w-full">
                 <div className="flex w-full flex-row space-x-1.5 justify-between m-1">
                     <div className="w-1/2 border rounded-sm overflow-clip ">
@@ -536,23 +503,8 @@ export default async function Page() {
                 </div>
             </div>
 
+            {/*이력조회*/}
             <History customerHistoryData={customerHistoryData} customerSMSHistoryData="" customerUnpaidHistoryData=""/>
-
-
-            {/*<button*/}
-            {/*    className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 generate-users-button"*/}
-            {/*    onClick={generateUsers}*/}
-            {/*>*/}
-            {/*    Generate Users*/}
-            {/*</button>*/}
-            {/*<button*/}
-            {/*    className={`ml-4 px-4 py-2 text-white rounded show-table-button ${isGenerated ? 'bg-green-500 hover:bg-green-600' : 'bg-green-200 cursor-not-allowed'}`}*/}
-            {/*    onClick={handleShowTable}*/}
-            {/*    disabled={!isGenerated}*/}
-            {/*>*/}
-            {/*    Show Table*/}
-            {/*</button>*/}
-            {/*{showTable && <UserTable users={users}/>}*/}
         </div>
     );
 }
