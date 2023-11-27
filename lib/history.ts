6ad46b4e6ac2f2d29f6cdc9e53a58220ce794acb
brucedev <whdtmek@gmail.com>
2023-11-27 20:33:58 +0900

    Data Fetch  - Bad gateway 디버깅 위해 log 찍어줌

--- lib/history.ts
+++ lib/history.ts
@@ -1,25 +1,41 @@
 import 'server-only'
 import {CustomerConsultationHistory} from "@/lib/types";
-import fs from "fs";
-import path from "path";
 
 export async function getHistoryData(): Promise<CustomerConsultationHistory[]> {
+    const s3Url = 'https://seoulforest946d11193b74484098fc836b62a80eb783206-dev.s3.ap-northeast-2.amazonaws.com/data.json'
+    const googleCloudUrl = 'https://storage.googleapis.com/skpoc/data.json';
 
-    const response = await fetch('https://storage.googleapis.com/skpoc/data.json',{cache: "no-cache"});
+    const response = await fetch(googleCloudUrl,{
+        cache: 'no-cache',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+        },
+        next: {
+            tags: ['api'],
+        }
+    });
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+
     const data = await response.json();
 
-    return data.map((item: any) => {
-        return {
-            consultationSequenceNumber: item.consultationSequenceNumber,
-            serviceNumber: item.serviceNumber,
-            consultationDate: item.consultationDate,
-            consultationTime: item.consultationTime,
-            serviceConsultationNumber: item.serviceConsultationNumber,
-            consultationType: item.consultationType,
-            notes: item.notes,
-            processStatus: item.processStatus,
-            consultantName: item.consultantName,
-            contactCategory: item.contactCategory,
-        };
-    });
+    return data.map(toHistory);
+}
+
+function toHistory(data: any): CustomerConsultationHistory {
+    return {
+        consultationSequenceNumber: data.consultationSequenceNumber,
+        serviceNumber: data.serviceNumber,
+        consultationDate: data.consultationDate,
+        consultationTime: data.consultationTime,
+        serviceConsultationNumber: data.serviceConsultationNumber,
+        consultationType: data.consultationType,
+        notes: data.notes,
+        processStatus: data.processStatus,
+        consultantName: data.consultantName,
+        contactCategory: data.contactCategory,
+    };
 }
