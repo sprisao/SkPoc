ba4637ee64c486c89a6869bb15dd55851f1579b5
sprisao <whdtmek@gmail.com>
2023-11-27 12:54:33 +0900

    type 교체

--- components/tables/CustomerServiceHistory/columns_customerServiceHistory.tsx
+++ components/tables/CustomerServiceHistory/columns_customerServiceHistory.tsx
@@ -1,8 +1,8 @@
 import {ColumnDef} from "@tanstack/react-table";
-import {CustomerServiceHistoryData} from "@/lib/types";
+import {CustomerConsultationHistory} from "@/lib/types";
 
 
-export const customerServiceHistoryColumns: ColumnDef<CustomerServiceHistoryData>[] = [
+export const customerServiceHistoryColumns: ColumnDef<CustomerConsultationHistory>[] = [
     {
         id: "consultationDate",
         accessorKey: "consultationDate",
