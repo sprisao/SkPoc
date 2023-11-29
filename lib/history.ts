71ecc480b319b0d01249866fc4c30dbef68ef061
sprisao <whdtmek@gmail.com>
2023-11-29 14:17:06 +0900

    Local Server 연동

--- lib/history.ts
+++ lib/history.ts
@@ -3,8 +3,9 @@ import {CustomerConsultationHistory} from "@/lib/types";
 export async function getHistoryData(): Promise<CustomerConsultationHistory[]> {
     const s3Url = 'https://seoulforest946d11193b74484098fc836b62a80eb783206-dev.s3.ap-northeast-2.amazonaws.com/data.json'
     const googleCloudUrl = 'https://storage.googleapis.com/skpoc/data.json';
+    const localUrl = 'http://localhost:8080/api/sk/GET_CSR_CNSL_HST_HISTORY';
 
-    const response = await fetch(googleCloudUrl,{
+    const response = await fetch(localUrl,{
         cache: 'no-cache',
         method: 'GET',
         headers: {
@@ -19,22 +20,27 @@ export async function getHistoryData(): Promise<CustomerConsultationHistory[]> {
         throw new Error(response.statusText);
     }
 
+
     const data = await response.json();
 
-    return data.map(toHistory);
+    // 'contents' 키에 해당하는 배열에 접근
+    const contents = data.contents;
+
+    // 각 항목을 'toHistory' 함수로 변환
+    return contents.map(toHistory);
 }
 
 function toHistory(data: any): CustomerConsultationHistory {
     return {
-        consultationSequenceNumber: data.consultationSequenceNumber,
-        serviceNumber: data.serviceNumber,
-        consultationDate: data.consultationDate,
-        consultationTime: data.consultationTime,
-        serviceConsultationNumber: data.serviceConsultationNumber,
-        consultationType: data.consultationType,
-        notes: data.notes,
-        processStatus: data.processStatus,
-        consultantName: data.consultantName,
-        contactCategory: data.contactCategory,
+        consultationSequenceNumber: data.CNSL_SEQ_NUM,
+        serviceNumber: data.SVC_CNSL_NUM,
+        consultationDate: data.CNSL_DT,
+        consultationTime: data.CNSL_TM,
+        serviceConsultationNumber: null, // Update this if you have a corresponding field in your data
+        consultationType: data.CNST_TYP,
+        notes: null, // Update this if you have a corresponding field in your data
+        processStatus: data.PROC_ST,
+        consultantName: data.CNSL_NM,
+        contactCategory: data.CONT_KND,
     };
 }
